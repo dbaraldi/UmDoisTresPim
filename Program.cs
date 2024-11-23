448ab8679ea206e40d04431d32eb87347e084e01
ETEC Adolpho Berezin <rubensjunior8216@gmail.com>
2024-11-22 22:18:17 -0300

    finalizei 1 2 3 pim

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int qtdExecutada = 0;
+int maxExecucoes = 40;
+
+while (qtdExecutada < maxExecucoes)
+{
+    qtdExecutada = qtdExecutada + 1;
+    Console.Write(qtdExecutada %4 == 0 ? "pim\n" : $"{qtdExecutada,1} ");
+}
